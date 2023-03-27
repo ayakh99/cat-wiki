31b5b1534ec1974de8a2171d35666fbcaae14eab
ayakh99 <ayaalaamohamed99@gmail.com>
2023-03-27 21:19:57 +0200

    project complete

--- src/layout/Main.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main({ children }) {
-  return (
-    <main className="mx-auto grid w-full max-w-screen-2xl place-content-center px-4.5 pt-16 pb-6 md:px-12 lg:py-25 lg:px-24">
-      {children}
-    </main>
-  );
-}
